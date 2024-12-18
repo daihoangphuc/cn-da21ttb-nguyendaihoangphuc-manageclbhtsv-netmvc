afc493705b0c38155997c86ac0f65efb5778a781
phuc <nguyendaihoangphuc24@gmail.com>
2024-12-18 10:15:38 +0700

    Xoa cot minh chung o hoat dong

---
+++ src/Migrations/20241218024935_xocotaminhchunghoatdong.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Manage_CLB_HTSV.Migrations
+{
+    public partial class xocotaminhchunghoatdong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinhChung",
+                table: "HoatDong");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MinhChung",
+                table: "HoatDong",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
